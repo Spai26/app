f563986237546839202c2c99c37dc101f0269105
Sergio Ai <sergioai.si93@gmail.com>
2022-01-30 13:17:17 -0500

    add colaboradores en el pie

--- pie.php
+++ pie.php
@@ -1,3 +1,13 @@
+<h1>Colaboradores</h1>
+    <ul>
+        <li><a href="#">Colaborador 1</a></li>
+        <li><a href="#">Colaborador 2</a></li>
+        <li><a href="#">Colaborador 3</a></li>
+        <li><a href="#">Colaborador 4</a></li>
+        <li><a href="#">Colaborador 5</a></li>
+
+    </ul>
+
 </body>
 
 </html>
