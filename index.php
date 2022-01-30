852a6f28be87c97a3abd4976a85f5a88e746c5d2
Sergio Ai <sergioai.si93@gmail.com>
2022-01-30 13:11:51 -0500

    Agregando texto en el indice

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php include("cabecera.php"); ?>
 
-<h1>Venta de index</h1>
+<h1>Menu Principal</h1>
+
+<p>Este fragmento de texto pone a prueba los cambios realizados por branch test1 </p>
+<ul>
+    <li>git branch (name)</li>
+    <li>git checkout (name)</li>    
+</ul>
+<p>Ambos comandos pueden ser representados por git checkout -b (name) </p>
 
 <?php include("pie.php"); ?>
